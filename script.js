33d93ab968ce7abcdc0119d70daac09d5b3da817
perwriters <perwriters@gmail.com>
2023-11-17 11:34:55 +0300

    Still Learning DOM

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const title = document.querySelectorAll(".heading");
-console.log(title);
-const list = document.querySelectorAll(".list");
-// console.log(list);
-for (i = 0; 1 < list.length; i++) {
-  list[i].style.fontSize = "5rem";
-}
-//Creating Elemets
-const ul = document.querySelector("ul");
-const li = document.createElement("li");
-ul.append(li);
+// const title = document.querySelectorAll(".heading");
+// console.log(title);
+// const list = document.querySelectorAll(".list");
+// // console.log(list);
+// for (i = 0; 1 < list.length; i++) {
+//   list[i].style.fontSize = "2rem";
+// }
+// //Creating Elemets
+// const ul = document.querySelector("ul");
+// const li = document.createElement("li");
+// ul.append(li);
+// // Examine the document Object
+// console.log(document.domain);
+// console.log(document.title);
+// console.log(document.URL);
+// console.log(document.head);
+
+// // selectetor
+// var contant = document.getElementById("head-text");
+// contant.innerText = "Peter"; //Maintains the style
+// contant.textContent = "Peter"; // Reads the whole content
+
+// // changing styles
+// contant.style.borderBottom = " solid 3px black";
+
+// // Get elemts by Class Name
+// var items = document.getElementsByClassName("list-items");
+// query Selector
+var git = document.querySelector("main-body");
+
+// Nodes and Parent Notes, Siblings
+// create element
+var newDiv = document.createElement("div");
