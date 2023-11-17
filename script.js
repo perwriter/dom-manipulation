b6a65c4fe3497da3326491bedb5aa35dca4bc8da
perwriters <perwriters@gmail.com>
2023-11-17 04:49:24 +0300

    Learning Dom

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const title = document.querySelectorAll(".heading");
 console.log(title);
 const list = document.querySelectorAll(".list");
-console.log(list);
+// console.log(list);
+for (i = 0; 1 < list.length; i++) {
+  list[i].style.fontSize = "5rem";
+}
+//Creating Elemets
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+ul.append(li);
